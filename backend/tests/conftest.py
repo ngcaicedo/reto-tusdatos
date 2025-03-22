312ca5b8ca00f93686fc5381db489156d42abdde
Nicolas Caicedo <ng.caicedo@uniandes.edu.co>
2025-03-22 12:07:40 -0500

    refactor: se crea un conftest para los fixture (blue)

---
+++ backend/tests/conftest.py
@@ -0,0 +1,22 @@
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
+from app.db.base import Base
+from app.models.event import Event
+
+@pytest.fixture(scope="function")
+def db():
+    engine = create_engine(
+        "sqlite:///:memory:",
+        connect_args={"check_same_thread": False},
+        poolclass=StaticPool,
+    )
+    TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    
+    Base.metadata.create_all(bind=engine)
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- backend/tests/test_event.py
+++ backend/tests/test_event.py
@@ -1,26 +1,4 @@
-import pytest
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.pool import StaticPool
 from sqlalchemy import inspect
-from app.db.base import Base
-from app.models.event import Event
-
-@pytest.fixture(scope="function")
-def db():
-    engine = create_engine(
-        "sqlite:///:memory:",
-        connect_args={"check_same_thread": False},
-        poolclass=StaticPool,
-    )
-    TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-    
-    Base.metadata.create_all(bind=engine)
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 
 def test_model_event(db):
