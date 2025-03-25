34a26c4b0921e15510cfeb2357254f1eaae63a54
Nicolas Caicedo <ng.caicedo@uniandes.edu.co>
2025-03-25 17:32:02 -0500

    test: se realizan pruebas para creacion de ponente desde interfaz (red)

--- frontend/src/app/session-managment/create-speaker/create-speaker.component.spec.ts
+++ frontend/src/app/session-managment/create-speaker/create-speaker.component.spec.ts
@@ -1,18 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreateSpeakerComponent } from './create-speaker.component';
+import { SessionService } from '../session.service';
+import { NotificationService } from '../../shared/services/notification.service';
+import { of, throwError } from 'rxjs';
 
 describe('CreateSpeakerComponent', () => {
   let component: CreateSpeakerComponent;
   let fixture: ComponentFixture<CreateSpeakerComponent>;
+  let mockSessionService: jasmine.SpyObj<SessionService>;
+  let mockNotifyService: jasmine.SpyObj<NotificationService>;
 
   beforeEach(async () => {
+    mockSessionService = jasmine.createSpyObj('SessionService', ['createSpeaker']);
+    mockNotifyService = jasmine.createSpyObj('NotificationService', ['success', 'error', 'info', 'warning']);
     await TestBed.configureTestingModule({
       imports: [CreateSpeakerComponent],
+      providers: [
+        { provide: SessionService, useValue: mockSessionService },
+        { provide: NotificationService, useValue: mockNotifyService }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(CreateSpeakerComponent);
     component = fixture.componentInstance;
+    component.ngOnInit();
+
+    component.speakerForm.setValue({
+      speakerName: 'test',
+    });
+
     fixture.detectChanges();
   });
 
@@ -30,5 +47,19 @@ describe('CreateSpeakerComponent', () => {
     expect(
       compiled.querySelector('input[formControlName="speakerName"]')
     ).toBeTruthy();
+  });
+
+  it('should create speaker', () => {
+    mockSessionService.createSpeaker.and.returnValue(of({ name: 'test' }));
+    component.createSpeaker();
+    expect(mockSessionService.createSpeaker).toHaveBeenCalledWith(component.speakerForm.value);
+  });
+
+  it('should create speaker error', () => {
+    let error = new Error('error');
+    mockSessionService.createSpeaker.and.returnValue(throwError(() => error));
+    component.createSpeaker();
+    expect(mockSessionService.createSpeaker).toHaveBeenCalledWith(component.speakerForm.value);
+    expect(mockNotifyService.error).toHaveBeenCalledWith(`Error al crear speaker: error`);
   });
 });
