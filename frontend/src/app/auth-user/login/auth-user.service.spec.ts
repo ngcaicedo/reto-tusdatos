440796c03a45f2c79ffe0829531dfe3d3335241f
Nicolas Caicedo <ng.caicedo@uniandes.edu.co>
2025-03-25 16:02:08 -0500

    test: se realizan pruebas para autenticar usuario en la aplicacion (red)

---
+++ frontend/src/app/auth-user/login/auth-user.service.spec.ts
@@ -0,0 +1,66 @@
+import { TestBed } from '@angular/core/testing';
+
+import { AuthUserService } from './auth-user.service';
+import {
+  HttpTestingController,
+  provideHttpClientTesting,
+} from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+
+describe('AuthUserService', () => {
+  let service: AuthUserService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        AuthUserService,
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ],
+    });
+    service = TestBed.inject(AuthUserService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should send a POST request to login', () => {
+    const user = {
+      email: 'test@gmail.com',
+      password: '123456',
+    };
+
+    const responseMock ={
+      user_id: 1,
+      token: 'token',
+      token_type: 'Bearer',
+      user:'test',
+      role: 'ASISTENTE',
+    }
+
+    service.login(user).subscribe({
+      next: (res) => {
+        expect(res).toEqual(responseMock);
+      },
+      error: (err) => {
+        console.error('error en la petición', err);
+      },
+    });
+
+    const req = httpMock.expectOne(
+      `${environment.apiUrl}/auth/login`
+    );
+
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual(user);
+
+  });
+});
