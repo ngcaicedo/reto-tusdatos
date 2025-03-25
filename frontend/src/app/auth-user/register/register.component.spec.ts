8c8de0097515688deb114bf72cab5eef13aea5c0
Nicolas Caicedo <ng.caicedo@uniandes.edu.co>
2025-03-25 10:53:52 -0500

    test: se realizan pruebas para formulario de registro de asistente

---
+++ frontend/src/app/auth-user/register/register.component.spec.ts
@@ -0,0 +1,53 @@
+/* tslint:disable:no-unused-variable */
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { RegisterComponent } from './register.component';
+
+describe('RegisterComponent', () => {
+  let component: RegisterComponent;
+  let fixture: ComponentFixture<RegisterComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [ RegisterComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RegisterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should show login form', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('form')).toBeTruthy();
+  });
+
+  it('should show name input', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('input[formControlName="name"]')).toBeTruthy();
+  });
+
+  it('should show phone input', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('input[formControlName="phone"]')).toBeTruthy();
+  });
+
+  it('should show email input', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('input[formControlName="email"]')).toBeTruthy();
+  });
+
+  it('should show password input', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('input[formControlName="password"]')).toBeTruthy();
+  });
+});
