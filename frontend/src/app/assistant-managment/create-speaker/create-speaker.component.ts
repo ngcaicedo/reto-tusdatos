e20621747e7eb61c6db34a7415b126469a107107
Nicolas Caicedo <ng.caicedo@uniandes.edu.co>
2025-03-25 11:56:31 -0500

    test: se realizan pruebas para formulario de creacion de ponentes (green)

--- frontend/src/app/assistant-managment/create-speaker/create-speaker.component.ts
+++ frontend/src/app/assistant-managment/create-speaker/create-speaker.component.ts
@@ -1,11 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-create-speaker',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './create-speaker.component.html',
-  styleUrl: './create-speaker.component.css'
+  styleUrl: './create-speaker.component.css',
 })
 export class CreateSpeakerComponent {
+  speakerForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.speakerForm = this.formBuilder.group({
+      speakerName: ['', Validators.required],
+    });
+  }
 }
