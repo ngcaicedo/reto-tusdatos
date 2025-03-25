7268e84affc5a8c5f74bf2f6c69d87174440f9d7
Nicolas Caicedo <ng.caicedo@uniandes.edu.co>
2025-03-25 11:50:26 -0500

    test: se realizan pruebas para formulario de creacion de ponentes (green)

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { CreateSpeakerComponent } from './assistant-managment/create-speaker/create-speaker.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'sessions/speaker/create', component: CreateSpeakerComponent, pathMatch: 'full'},
+];
