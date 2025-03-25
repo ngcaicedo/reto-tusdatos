bbb247b7164bf21cc35c089eeb10a17b38a22826
Nicolas Caicedo <ng.caicedo@uniandes.edu.co>
2025-03-25 01:03:10 -0500

    test: se realizan pruebas para create, update and read de sessions (red)

--- backend/tests/test_session.py
+++ backend/tests/test_session.py
@@ -106,3 +106,153 @@ def test_get_speaker(client, auth_header, user_factory):
     data_response = response.json()
     assert data_response.get("id") == speaker.get("id")
     assert data_response.get("name") == speaker.get("name")
+    
+def test_create_session(client, auth_header, user_factory):
+    """ Función para crear una sesión """
+    user_login = user_factory.login_user(role=RoleEnum.ORGANIZADOR)
+    data_speaker = {
+        "name": faker.name(),
+    }
+    headers = auth_header(user_login)
+    response = client.post("sessions/create/speaker/", json=data_speaker, headers=headers)
+    speaker = response.json()
+    data = {
+        "name": faker.name(),
+        "description": faker.text(),
+        "date": faker.date_time_this_year(),
+        "speaker_id": speaker.get("id"),
+    }
+    response = client.post("sessions/create/", json=data, headers=headers)
+    assert response.status_code == 201
+    data_response = response.json()
+    assert data_response.get("id") is not None
+    assert data_response.get("name") == data["name"]
+    assert data_response.get("description") == data["description"]
+    assert data_response.get("date") == data["date"]
+    assert data_response.get("speaker_id") == speaker.get("id")
+    
+    
+def test_create_session_duplicate(client, auth_header, user_factory):
+    """ Función para crear una sesión duplicada """
+    user_login = user_factory.login_user(role=RoleEnum.ORGANIZADOR)
+    data_speaker = {
+        "name": faker.name(),
+    }
+    headers = auth_header(user_login)
+    response = client.post("sessions/create/speaker/", json=data_speaker, headers=headers)
+    speaker = response.json()
+    data = {
+        "name": faker.name(),
+        "description": faker.text(),
+        "date": faker.date_time_this_year(),
+        "speaker_id": speaker.get("id"),
+    }
+    response = client.post("sessions/create/", json=data, headers=headers)
+    response = client.post("sessions/create/", json=data, headers=headers)
+    assert response.status_code == 409
+    data_response = response.json()
+    assert data_response.get("detail") == "La sesión ya se encuentra registrada"
+    
+def test_update_session(client, auth_header, user_factory):
+    """ Función para actualizar una sesión """
+    user_login = user_factory.login_user(role=RoleEnum.ORGANIZADOR)
+    data_speaker = {
+        "name": faker.name(),
+    }
+    headers = auth_header(user_login)
+    response = client.post("sessions/create/speaker/", json=data_speaker, headers=headers)
+    speaker = response.json()
+    data = {
+        "name": faker.name(),
+        "description": faker.text(),
+        "date": faker.date_time_this_year(),
+        "speaker_id": speaker.get("id"),
+    }
+    response = client.post("sessions/create/", json=data, headers=headers)
+    session = response.json()
+    data2 = {
+        "name": faker.name(),
+        "description": faker.text(),
+        "date": faker.date_time_this_year(),
+        "speaker_id": speaker.get("id"),
+    }
+    response = client.put(f"sessions/update/{session.get('id')}", json=data2, headers=headers)
+    assert response.status_code == 200
+    data_response = response.json()
+    assert data_response.get("id") == session.get("id")
+    assert data_response.get("name") == data2["name"]
+    assert data_response.get("description") == data2["description"]
+    assert data_response.get("date") == data2["date"]
+    assert data_response.get("speaker_id") == speaker.get("id")
+    
+def test_update_session_not_found(client, auth_header, user_factory):
+    """ Función para actualizar una sesión no encontrada """
+    user_login = user_factory.login_user(role=RoleEnum.ORGANIZADOR)
+    data_speaker = {
+        "name": faker.name(),
+    }
+    headers = auth_header(user_login)
+    response = client.post("sessions/create/speaker/", json=data_speaker, headers=headers)
+    speaker = response.json()
+    data = {
+        "name": faker.name(),
+        "description": faker.text(),
+        "date": faker.date_time_this_year(),
+        "speaker_id": speaker.get("id"),
+    }
+    response = client.put(f"sessions/update/1000", json=data, headers=headers)
+    assert response.status_code == 404
+    data_response = response.json()
+    assert data_response.get("detail") == "La sesión no se encuentra registrada"
+    
+    
+def test_get_sessions(client, auth_header, user_factory):
+    """ Función para obtener todas las sesiones """
+    user_login = user_factory.login_user(role=RoleEnum.ORGANIZADOR)
+    data_speaker = {
+        "name": faker.name(),
+    }
+    headers = auth_header(user_login)
+    response = client.post("sessions/create/speaker/", json=data_speaker, headers=headers)
+    speaker = response.json()
+    data = {
+        "name": faker.name(),
+        "description": faker.text(),
+        "date": faker.date_time_this_year(),
+        "speaker_id": speaker.get("id"),
+    }
+    response = client.post("sessions/create/", json=data, headers=headers)
+    assert response.status_code == 201
+    
+    response = client.get("sessions/", headers=headers)
+    assert response.status_code == 200
+    data_response = response.json()
+    assert isinstance(data_response, list)
+    assert len(data_response) > 0
+    assert data_response[0].get("id") is not None
+    
+def test_get_session(client, auth_header, user_factory):
+    """ Función para obtener una sesión """
+    user_login = user_factory.login_user(role=RoleEnum.ORGANIZADOR)
+    data_speaker = {
+        "name": faker.name(),
+    }
+    headers = auth_header(user_login)
+    response = client.post("sessions/create/speaker/", json=data_speaker, headers=headers)
+    speaker = response.json()
+    data = {
+        "name": faker.name(),
+        "description": faker.text(),
+        "date": faker.date_time_this_year(),
+        "speaker_id": speaker.get("id"),
+    }
+    response = client.post("sessions/create/", json=data, headers=headers)
+    session = response.json()
+    response = client.get(f"sessions/{session.get('id')}", headers=headers)
+    assert response.status_code == 200
+    data_response = response.json()
+    assert data_response.get("id") == session.get("id")
+    assert data_response.get("name") == session.get("name")
+    assert data_response.get("description") == session.get("description")
+    assert data_response.get("date") == session.get("date")
+    assert data_response.get("speaker_id") == speaker.get("id")
