04cc8264fd3fe760b7bb2079d7b4d4a07e37dc02
Nicolas Caicedo <ng.caicedo@uniandes.edu.co>
2025-03-26 11:26:41 -0500

    test: creacion de servicio para registrar el usuario (green)

--- frontend/src/app/auth-user/auth.service.ts
+++ frontend/src/app/auth-user/auth.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private apiUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  registerUser(user: {
+    name: string;
+    email: string;
+    phone: string;
+    password: string;
+    role: string;
+  }) {
+    return this.http.post(`${this.apiUrl}/auth/register`, user);
+  }
 }
