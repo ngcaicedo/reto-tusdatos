f8e505d521dd7bc97fc24574ba1b58a50c618b77
Nicolas Caicedo <ng.caicedo@uniandes.edu.co>
2025-03-25 19:42:18 -0500

    test: se crea test para formulario de creacion de eventos (green)

--- frontend/src/app/event-managment/create-event/create-event.component.ts
+++ frontend/src/app/event-managment/create-event/create-event.component.ts
@@ -1,11 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-event',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './create-event.component.html',
   styleUrl: './create-event.component.css'
 })
 export class CreateEventComponent {
+  eventForm!: FormGroup;
 
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
+
+  ngOnInit(): void {
+    this.eventForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      capacity: ['', Validators.required],
+      date_start: ['', Validators.required],
+      date_end: ['', Validators.required],
+      location: ['', Validators.required]
+    });
+  }
+
+  createEvent(){}
 }
