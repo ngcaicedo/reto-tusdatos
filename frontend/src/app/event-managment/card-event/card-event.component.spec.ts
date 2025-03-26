a72c3438aa11ec1dabf45bb7513a45314659ab01
Nicolas Caicedo <ng.caicedo@uniandes.edu.co>
2025-03-26 00:18:56 -0500

    test: creacion de tarjetas para eventos (red)

---
+++ frontend/src/app/event-managment/card-event/card-event.component.spec.ts
@@ -0,0 +1,48 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CardEventComponent } from './card-event.component';
+
+describe('CardEventComponent', () => {
+  let component: CardEventComponent;
+  let fixture: ComponentFixture<CardEventComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CardEventComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CardEventComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it(`should have show card event`, () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.card')).toBeTruthy();
+  });
+
+  it(`should have show card event title`, () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('#title')).toBeTruthy();
+  });
+
+  it(`should have show card event location`, () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('#location')).toBeTruthy();
+  });
+
+  it(`should have show card event date`, () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('#date')).toBeTruthy();
+  });
+
+  it(`should have show card event img`, () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.card-img-top')).toBeTruthy();
+  });
+});
