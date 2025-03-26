709d9b03b609f0640a8cddd95e9d2d1c6470e69d
Nicolas Caicedo <ng.caicedo@uniandes.edu.co>
2025-03-26 00:26:08 -0500

    test: creacion de tarjetas para eventos (green)

--- frontend/src/app/event-managment/card-event/card-event.component.ts
+++ frontend/src/app/event-managment/card-event/card-event.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-event',
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './card-event.component.css'
 })
 export class CardEventComponent {
-
+  @Input() title!: string;
+  @Input() location!: string;
+  @Input() date!: string;
+  @Input() img!: string;
+  @Input() state!: string;
 }
