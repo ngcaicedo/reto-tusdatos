7a5a97533f045308fdcd67b4bbe2754ab876fd22
Nicolas Caicedo <ng.caicedo@uniandes.edu.co>
2025-03-25 20:00:48 -0500

    test: se crea test del servicio de crear evento (green)

--- frontend/src/app/event-managment/event.service.ts
+++ frontend/src/app/event-managment/event.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EventService {
+  private apiUrl = environment.apiUrl;
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  createEvent(event: {
+    name: string;
+    description: string;
+    capacity: number;
+    state: string;
+    date_start: string;
+    date_end: string;
+    location: string;
+    user_created_id: number;
+    session_ids: number[];
+  }): Observable<any> {
+    return this.http.post(`${this.apiUrl}/events/create`, event);
+  }
 }
